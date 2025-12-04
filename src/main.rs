891b20c0fa85797346e4fb73972c7fdc1fb1930c
busterw <busterw@hotmail.co.uk>
2025-12-04 21:30:17 +0000

    day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -9,30 +9,52 @@ fn main() {
     day1();
 }
 
-pub fn day1(){
+pub fn day1() {
     let instructions = instructions_from_file();
 
-    let mut current_pos = 50;
+    let mut current_pos: i32 = 50;
     let mut zero_count = 0;
+    let mut pass_zero_count = 0;
 
     for instruction in instructions {
         match instruction.direction {
             'L' => {
-                current_pos -= instruction.amount;
+                for x in 0..instruction.amount {
+                    current_pos -= 1;
+                    current_pos = current_pos.rem_euclid(100);
+                    let final_step = x == instruction.amount - 1;
+                    if current_pos == 0 {
+                        if final_step {
+                            zero_count += 1;        // lands on zero
+                        } else {
+                            pass_zero_count += 1;   // passes through zero
+                        }
+                }
             }
-            'R' => current_pos += instruction.amount,
-            _ => todo!(),
         }
-        if current_pos % 100 == 0 {
-            zero_count += 1
+            'R' => {
+                for x in 0..instruction.amount {
+                    current_pos += 1;
+                    current_pos = current_pos.rem_euclid(100);
+                    let final_step = x == instruction.amount - 1;
+                    if current_pos == 0 {
+                        if final_step {
+                            zero_count += 1;        // lands on zero
+                        } else {
+                            pass_zero_count += 1;   // passes through zero
+                        }
+                }
+            }
+        }
+            _ => todo!(),
         }
     }
 
-    println!("{}", zero_count)
+    println!("{}", zero_count + pass_zero_count);
 }
 
 pub fn instructions_from_file() -> Vec<Instructions> {
-    let lines = read_lines("./input.txt").unwrap();
+    let lines = read_lines("./inputday1.txt").unwrap();
 
     lines
         .map(|x| {
